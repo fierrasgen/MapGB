8522847251fdf7e0db3afe2e34af6c5cf7e319eb
Fierrasgen <Look00@inbox.ru>
2021-09-03 19:10:27 +0300

    Add homework 4. Secure text.

--- MapsGB/AppDelegate.swift
+++ MapsGB/AppDelegate.swift
@@ -11,7 +11,7 @@ import GoogleMaps
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var visualEffectView = UIVisualEffectView()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyB9gfRhEKbpQK-1n0s0uJXZZ7OIzbLXyVQ")
@@ -19,6 +19,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
+    
+//    var window: UIWindow?
+//    var blurViewTag: Int {
+//            return 999999
+//        }
+//
+//    func applicationWillResignActive(_ application: UIApplication) {
+//        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.light)
+//        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+//        blurEffectView.frame = window!.frame
+//        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        blurEffectView.tag = blurViewTag
+//
+//
+//
+//        self.window?.addSubview(blurEffectView)
+//       }
+//
+//       func applicationDidBecomeActive(_ application: UIApplication) {
+//        self.window?.viewWithTag(blurViewTag)?.removeFromSuperview()
+//       }
+//   }
+//
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -32,6 +55,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    var window: UIWindow?
 
-}
+        func applicationWillResignActive(_ application: UIApplication) {
+            addBlurViews()
+        }
+
+        func applicationDidBecomeActive(_ application: UIApplication) {
+            removeBlurViews()
+        }
+    }
+
+    private extension AppDelegate {
+        var blurViewTag: Int {
+            return 999999
+        }
+
+        func addBlurViews() {
+            for window in UIApplication.shared.windows {
+                let blurEffect: UIBlurEffect
+                if #available(iOS 14.0, *) {
+                    blurEffect = UIBlurEffect(style: .systemUltraThinMaterial)
+                } else {
+                    blurEffect = UIBlurEffect(style: .light)
+                }
+                let blurEffectView = UIVisualEffectView(effect: blurEffect)
+                blurEffectView.frame = window.frame
+                blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                blurEffectView.tag = blurViewTag
+                window.addSubview(blurEffectView)
+            }
+        }
+
+        func removeBlurViews() {
+            for window in UIApplication.shared.windows {
+                if let blurView = window.viewWithTag(blurViewTag) {
+                    blurView.removeFromSuperview()
+                }
+            }
+        }
+    }
+
+
+
 
