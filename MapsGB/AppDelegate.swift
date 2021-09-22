f745f2d7f4e7e27839e2080de78a0cf61d8d78bf
Fierrasgen <Look00@inbox.ru>
2021-09-22 14:51:17 +0300

    HW7. Add notification.

--- MapsGB/AppDelegate.swift
+++ MapsGB/AppDelegate.swift
@@ -10,26 +10,57 @@ import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var visualEffectView = UIVisualEffectView()
-
+    let center = UNUserNotificationCenter.current()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyB9gfRhEKbpQK-1n0s0uJXZZ7OIzbLXyVQ")
+        
+        
+        // Request auth
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { [ weak self ] granted, _ in
+            
+            self?.sendNotification()
+        }
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
     
+    func sendNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "MapsGb"
+        content.subtitle = "Attenstion"
+        content.body = "Please, open App"
+        content.badge = 10
+        content.sound = .default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        let request = UNNotificationRequest(
+            identifier: "reminder",
+            content: content,
+            trigger: trigger
+        )
+        
+        center.add(request) { error in
+            if let error = error {
+                print("Notification shedule \(error)")
+            }
+        }
+        
+        // MARK: UISceneSession Lifecycle
+        
+        
+        func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+            
+            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        }
+        
+        func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+            
+        }
+    }
     
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 }
