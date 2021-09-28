ff2148e329c1b4208cfd8062ff4ab46f9751223f
Fierrasgen <Look00@inbox.ru>
2021-09-28 17:30:11 +0300

    Add lesson 8. Added only the code from the manual.

--- MapsGB/Flows/Main/MainViewController.swift
+++ MapsGB/Flows/Main/MainViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 
+
 final class MainViewController: UIViewController {
     
     @IBAction func showMap(_ sender: Any) {
@@ -18,4 +19,49 @@ final class MainViewController: UIViewController {
         UserDefaults.standard.set(false, forKey: "isLogin")
         performSegue(withIdentifier: "logout", sender: sender)
     }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+                
+        else {
+            return
+        }
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
 }
+
+extension MainViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let image = extractImage(from: info) {
+            imageView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    private func extractImage(from info: [UIImagePickerController.InfoKey: Any]) -> UIImage? {
+   
+        if let image = info[.editedImage] as? UIImage {
+                return image
+        }
+        if let image = info[.originalImage] as? UIImage {
+            return image
+        }
+        
+        return nil
+    }
+}
+
+
